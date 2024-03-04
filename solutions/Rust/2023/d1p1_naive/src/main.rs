84066abe44450442755399d41308fd4ac9354e29
CordlessCoder <>
2024-03-04 15:29:29 +0000

    Simplify Rust d1p1 naive

--- solutions/Rust/2023/d1p1_naive/src/main.rs
+++ solutions/Rust/2023/d1p1_naive/src/main.rs
@@ -5,12 +5,18 @@ fn solution(input: &str) -> u32 {
         .as_bytes()
         .split(|&b| b == b'\n')
         .map(|line| {
-            let mut iter = line.iter().copied().filter(|&b| b <= b'0');
+            let mut iter = line
+                .iter()
+                .copied()
+                .map(|b| b.wrapping_sub(b'0'))
+                .filter(|&b| b <= 9);
             let first = iter.next().unwrap_or(0);
             let last = iter.next_back().unwrap_or(first);
-            ((first & 0xf) as u32, (last & 0xf) as u32)
+            (first, last)
         })
-        .fold((0, 0), |(first, last), (f, l)| (first + f, last + l));
+        .fold((0, 0), |(first, last), (f, l)| {
+            (first + f as u32, last + l as u32)
+        });
     first * 10 + last
 }
 
