7233eb9caa1a7bf8e5968f55c097a8106b0934c4
Aimar Ibarra <greenerclay@gmail.com>
2024-03-05 22:37:35 +0100

    Haskell implementation added

---
+++ solutions/Haskell/2023/naive/src/Lib.hs
@@ -0,0 +1,12 @@
+module Lib (adventOfCode) where
+
+import Data.Char
+
+adventOfCode :: String -> Int
+adventOfCode s = sum $ map readCalibration (lines s)
+
+readCalibration :: String -> Int
+readCalibration s = readCalibration' $ map digitToInt (filter isDigit s)
+  where
+    readCalibration' [] = 0
+    readCalibration' l  = (head l) * 10 + (last l)
