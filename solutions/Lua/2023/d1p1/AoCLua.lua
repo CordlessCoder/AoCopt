2cc816cf2a0817c7a71aeca9bb6d59945b3266c0
Deity <59446525+saint-deity@users.noreply.github.com>
2024-03-07 02:25:28 +1000

    Add files via upload

---
+++ solutions/Lua/2023/d1p1/AoCLua.lua
@@ -0,0 +1,25 @@
+local uv = require "uv"
+local input = io.read ("*all")
+local start = uv.hrtime ()
+
+local result = 0
+local lines = { }
+
+for line in input:gmatch ("[^\r\n]+") do
+    table.insert (lines, line)
+end
+
+function getFirstAndLastNumber (str)
+    local first = string.match (str, "%d+")
+    local last = string.match (string.reverse (str), "%d+")
+    return first, last
+end
+
+for i = 1, #lines do
+    local first, last = getFirstAndLastNumber (lines [i])
+    result = result + ((first*10) + last)
+end
+
+local stop = uv.hrtime ()
+print ("Result: "..result)
+print ("Benchmark: "..(stop - start)..'ns')
