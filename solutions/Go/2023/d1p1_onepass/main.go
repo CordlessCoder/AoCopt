4facaa5e5eeb52d2cf703680a2d7d9ef2c1373d7
CordlessCoder <>
2024-03-06 17:08:54 +0000

    fix Go formatting

--- solutions/Go/2023/d1p1_onepass/main.go
+++ solutions/Go/2023/d1p1_onepass/main.go
@@ -19,7 +19,7 @@ func main() {
 		reader = os.Stdin
 	}
 
-    input, err := io.ReadAll(reader)
+	input, err := io.ReadAll(reader)
 	if err != nil {
 		panic(err)
 	}
@@ -28,30 +28,30 @@ func main() {
 
 	sum := 0
 
-    for len(input) > 0 {
-        var first = 0
-        for len(input) > 0 && input[0] != '\n' {
-            num := input[0] - '0'
-            input = input[1:]
-            if 0<= num && num <= 9 {
-                first = int(num)
-                break
-            }
-        }
-        var last = first
-        for len(input) > 0 && input[0] != '\n' {
-            num := input[0] - '0'
-            input = input[1:]
-            if 0<= num && num <= 9 {
-                last = int(num)
-            }
-        }
-        if len(input) > 0 {
-            // Consume newline
-            input = input[1:]
-        }
-        sum += first * 10 + last
-    }
+	for len(input) > 0 {
+		var first = 0
+		for len(input) > 0 && input[0] != '\n' {
+			num := input[0] - '0'
+			input = input[1:]
+			if 0 <= num && num <= 9 {
+				first = int(num)
+				break
+			}
+		}
+		var last = first
+		for len(input) > 0 && input[0] != '\n' {
+			num := input[0] - '0'
+			input = input[1:]
+			if 0 <= num && num <= 9 {
+				last = int(num)
+			}
+		}
+		if len(input) > 0 {
+			// Consume newline
+			input = input[1:]
+		}
+		sum += first*10 + last
+	}
 
 	duration := time.Since(start).Nanoseconds()
 
