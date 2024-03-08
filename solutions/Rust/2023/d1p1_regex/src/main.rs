319bc16f4eb7b34b3ccf8f55472817fcc9c7a474
CordlessCoder <>
2024-03-08 10:45:38 +0000

    speed up Rust regex

--- solutions/Rust/2023/d1p1_regex/src/main.rs
+++ solutions/Rust/2023/d1p1_regex/src/main.rs
@@ -1,32 +1,39 @@
 use std::sync::OnceLock;
 
-static REGEX: OnceLock<regex::bytes::Regex> = OnceLock::new();
+static REGEX: OnceLock<[regex::bytes::Regex; 2]> = OnceLock::new();
 
 fn solve(input: &str) -> u32 {
-    let regex = REGEX.get().expect("Regex was not compiled");
-    let lines = regex
+    let [first, last] = REGEX.get().unwrap();
+    let first = first
         .captures_iter(input.as_bytes())
-        .map(|cap| {
-            let first = cap.get(1).unwrap();
-            let last = cap.get(2).unwrap_or(first);
-            (first.as_bytes(), last.as_bytes())
-        })
-        .map(|(first, last)| (first[0], last[0]))
-        .map(|(first, last)| (first.wrapping_sub(b'0'), last.wrapping_sub(b'0')));
+        .map(|cap| cap.get(1).unwrap().as_bytes()[0])
+        .map(|b| b.wrapping_sub(b'0'));
+    let last = last
+        .captures_iter(input.as_bytes())
+        .map(|cap| cap.get(1).unwrap().as_bytes()[0])
+        .map(|b| b.wrapping_sub(b'0'));
+    let lines = first.zip(last);
 
     let (sum_first, sum_last) = lines.fold((0, 0), |(sum_first, sum_last), (first, last)| {
         (sum_first + first as u32, sum_last + last as u32)
     });
+
     sum_first * 10 + sum_last
 }
 
 fn main() {
     REGEX.get_or_init(|| {
-        regex::bytes::RegexBuilder::new(r#"^*\D*(\d)(?:.*(\d))?.*$"#)
+        let first = regex::bytes::RegexBuilder::new(r#"^\D*(\d)"#)
             .unicode(false)
             .multi_line(true)
             .build()
-            .unwrap()
+            .unwrap();
+        let last = regex::bytes::RegexBuilder::new(r#"(\d)\D*$"#)
+            .unicode(false)
+            .multi_line(true)
+            .build()
+            .unwrap();
+        [first, last]
     });
     aoc_util::hook_solution(solve)
 }
