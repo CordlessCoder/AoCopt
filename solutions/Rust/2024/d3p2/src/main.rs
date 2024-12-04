1a966a82285b266f96ec91c255c10220b27a51a2
CordlessCoder <>
2024-12-04 02:22:30 +0000

    Complete 2024 day 3 part 2 solution

--- solutions/Rust/2024/d3p2/src/main.rs
+++ solutions/Rust/2024/d3p2/src/main.rs
@@ -1,8 +1,17 @@
-use winnow::{ascii, combinator, error::InputError, Parser};
+use winnow::{ascii, combinator, error::InputError, token, Parser};
 
 fn solution(mut input: &str) -> u32 {
     let int = ascii::dec_uint::<_, u32, InputError<_>>;
-    let mut parser = combinator::delimited("mul(", combinator::separated_pair(int, ',', int), ')');
+    let parser = combinator::delimited("mul(", combinator::separated_pair(int, ',', int), ')');
+    let disabled = (
+        "don't()",
+        combinator::alt((
+            token::take_until::<_, _, InputError<_>>(0.., "do()"),
+            combinator::rest_len.flat_map(|len| token::take(len.saturating_sub(1))),
+        )),
+        combinator::opt("do()"),
+    );
+    let mut parser = combinator::preceded(combinator::opt(disabled), parser);
     let mut sum = 0;
     while !input.is_empty() {
         let Ok((a, b)) = parser.parse_next(&mut input) else {
